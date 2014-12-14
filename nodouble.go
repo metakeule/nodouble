42d968813da856c94b942f4f38effdba089487dd
Marc Rene Arns <arns@stridor.net>
2014-12-14 20:35:00 +0100

    better wording

--- nodouble.go
+++ nodouble.go
@@ -7,20 +7,20 @@ import (
 
 type info struct {
 	version string
-	file string
-	line int
+	file    string
+	line    int
 }
 
 var pkgs = map[string]info{}
 
 func AddPackage(path, version string) {
-	_,file,line,_ := runtime.Caller(0)
+	_, file, line, _ := runtime.Caller(0)
 	i, has := pkgs[path]
 	if has {
-		panic(fmt.Sprintf( "can't load package %s v%s as requested in %s:%v; already loaded version %s as requested from %s:%v",
-path,version, file,line,
-i.version, i.file, i.line,
+		panic(fmt.Sprintf("can't load package %s version %s as requested in %s:%v; already loaded version %s as requested from %s:%v",
+			path, version, file, line,
+			i.version, i.file, i.line,
 		))
 	}
-	pkgs[path] = info{version,file,line}
+	pkgs[path] = info{version, file, line}
 }
